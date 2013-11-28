67cb4705f9acd006c2e6f7aad36806dba301ac3c
Usman Bashir <me@usmanbashir.com>
2013-11-28 08:49:05 +0300

    Add a system tray icon and a popup menu that lets users exit the app.

--- kabegami.rb
+++ kabegami.rb
@@ -5,8 +5,14 @@ import javax.swing.JPanel
 import javax.swing.JButton
 import javax.swing.JOptionPane
 import java.lang.System
+import java.awt.SystemTray
+import java.awt.Toolkit
+import java.awt.TrayIcon
+import java.awt.PopupMenu
+import java.awt.MenuItem
 
 APP_NAME = "Kabegami"
+APP_ICON = "icon.png"
 
 class Kabegami < JFrame
 
@@ -39,6 +45,35 @@ class Kabegami < JFrame
     
     panel.add btnClose
     
+
+    # Setup the system tray.
+    
+    if SystemTray.isSupported
+      tray          = SystemTray.getSystemTray
+      icon          = Toolkit.getDefaultToolkit.getImage APP_ICON
+
+      popupMenu     = PopupMenu.new
+
+      menuItemExit  = MenuItem.new "Exit"
+      menuItemExit.addActionListener do |e|
+        confirm = JOptionPane.showConfirmDialog(
+                    nil, 
+                    "Are you sure dude, you wanna call it a day?",
+                    "Just Checking?",
+                    JOptionPane::YES_NO_OPTION
+                  )
+
+        System.exit 0 if confirm == JOptionPane::YES_OPTION
+      end
+
+      popupMenu.add menuItemExit
+      
+      trayIcon      = TrayIcon.new icon, APP_NAME, popupMenu
+
+      tray.add trayIcon
+    end
+    
+
     # Everthing is setup, we can render the window now.
     self.setVisible true
   end
